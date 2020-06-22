eaa3728cd7fbbf80228bb51b123d19eb5ec14998
keisuke.teraoka <keisuke.teraoka@asoview.co.jp>
2020-06-22 12:49:35 +0900

    modify entity

--- src/main/java/dev/tobachi/sample/SampleApplication.java
+++ src/main/java/dev/tobachi/sample/SampleApplication.java
@@ -2,8 +2,10 @@ package dev.tobachi.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("dev.tobachi.sample.domain")
 public class SampleApplication {
 
     public static void main(String[] args) {
