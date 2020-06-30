817768032081b733e359f0e46fd49bb4bb07c236
keisuke.teraoka <keisuke.teraoka@asoview.co.jp>
2020-06-30 12:43:44 +0900

    modify configuration

--- src/main/java/dev/tobachi/sample/domain/User.java
+++ src/main/java/dev/tobachi/sample/domain/User.java
@@ -5,11 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.time.LocalDate;
 
 @Entity
+@Table(name = "user")
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
@@ -17,9 +20,15 @@ import java.time.LocalDate;
 public class User {
 
     @Id
+    @Column(name="id")
+    private Integer id;
+
+    @Column(name="name")
     private String name;
 
+    @Column(name="age")
     private Integer age;
 
+    @Column(name="birthday")
     private LocalDate birthday;
 }
